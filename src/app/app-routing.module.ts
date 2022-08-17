e1e8cd000a80ee59208e4645f1b573a69b2b5785
gerasalta <geragutierrezsalta@gmail.com>
2022-08-17 16:44:41 -0300

    admin page started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'admin', component: AdminComponent},
   {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
+import { AdminComponent } from './admin/admin.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule,
